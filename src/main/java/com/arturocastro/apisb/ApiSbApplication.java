d57facf26d90bd4717e4bd44945bba18bd4f21db
AJoelCastro <misthorn33@gmail.com>
2025-09-20 23:12:44 -0500

    first commit

---
+++ src/main/java/com/arturocastro/apisb/ApiSbApplication.java
@@ -0,0 +1,13 @@
+package com.arturocastro.apisb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSbApplication.class, args);
+    }
+
+}
