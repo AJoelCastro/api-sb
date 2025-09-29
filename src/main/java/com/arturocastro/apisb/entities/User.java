8cee25ce6bcf70535cd6a1bfc2f1dd70e19f271e
AJoelCastro <misthorn33@gmail.com>
2025-09-28 20:59:11 -0500

    values to user

--- src/main/java/com/arturocastro/apisb/entities/User.java
+++ src/main/java/com/arturocastro/apisb/entities/User.java
@@ -1,5 +1,48 @@
 package com.arturocastro.apisb.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+
+    public User() {
+    }
+    public User(Long id, String name, String lastName, String email) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 }
---
+++ src/main/java/com/arturocastro/apisb/repository/User.java
@@ -0,0 +1,5 @@
+package com.arturocastro.apisb.repository;
+
+public interface User {
+
+}
