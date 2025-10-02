df927f6a9ddef90a672fc95f2132c5b96e04bc7f
Castro Reyes Arturo Joel <140865789+AJoelCastro@users.noreply.github.com>
2025-10-01 23:32:09 -0500

    Update UserController.java

--- src/main/java/com/arturocastro/apisb/controller/UserController.java
+++ src/main/java/com/arturocastro/apisb/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import com.arturocastro.apisb.entitie.User;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
