3408789a78e783cd8f86216fef257cbd8b48cd91
AJoelCastro <misthorn33@gmail.com>
2025-10-06 23:25:41 -0500

    product controller

--- src/main/java/com/arturocastro/apisb/sevice/ProductService.java
+++ src/main/java/com/arturocastro/apisb/sevice/ProductService.java
@@ -1,5 +1,6 @@
 package com.arturocastro.apisb.sevice;
 
+import com.arturocastro.apisb.entitie.Product;
 import com.arturocastro.apisb.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +13,14 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
+    public Product save(Product product){
+        if(product.getId() != null){
+            throw new IllegalArgumentException("El id no debe enviarse por el cliente, Mongo DB lo generará automaticamente");
+        }
+        if(productRepository.findAll().stream().anyMatch(p -> p.getName().equalsIgnoreCase(product.getName()))){
+            throw new IllegalArgumentException("Ya existe un producto con ese nombre");
+        }
+        return productRepository.save(product);
+    }
 
 }
