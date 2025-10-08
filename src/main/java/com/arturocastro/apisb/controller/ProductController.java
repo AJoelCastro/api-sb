4ae2701f40300ba325b4b20399d9523bd06b3708
AJoelCastro <misthorn33@gmail.com>
2025-10-07 23:54:43 -0500

    product controller

--- src/main/java/com/arturocastro/apisb/controller/ProductController.java
+++ src/main/java/com/arturocastro/apisb/controller/ProductController.java
@@ -1,7 +1,12 @@
 package com.arturocastro.apisb.controller;
 
+import com.arturocastro.apisb.entitie.Product;
 import com.arturocastro.apisb.sevice.ProductService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
@@ -10,6 +15,14 @@ public class ProductController {
 
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    public ResponseEntity<Optional<List<Product>>> getAll(){
+        Optional <List<Product>> products = Optional.ofNullable(productService.findAll());
+        if(products.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(products);
     }
 
 }
