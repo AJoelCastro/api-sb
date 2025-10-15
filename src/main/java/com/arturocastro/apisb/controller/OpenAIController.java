6eafd96405bbbb118b6d514665e7d3f680c7f6d1
AJoelCastro <misthorn33@gmail.com>
2025-10-14 23:45:05 -0500

    openai service

---
+++ src/main/java/com/arturocastro/apisb/controller/OpenAIController.java
@@ -0,0 +1,7 @@
+package com.arturocastro.apisb.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OpenAIController {
+}
--- src/main/java/com/arturocastro/apisb/sevice/OpenAIService.java
+++ src/main/java/com/arturocastro/apisb/sevice/OpenAIService.java
@@ -1,4 +1,19 @@
 package com.arturocastro.apisb.sevice;
 
+import com.openai.client.OpenAIClient;
+import com.openai.client.okhttp.OpenAIOkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OpenAIService {
+
+    private final OpenAIClient client;
+
+    public OpenAIService(@Value("${openai.api.key}") String apiKey) {
+        client = new OpenAIOkHttpClient.Builder()
+                .apiKey(apiKey)
+                .build();
+    }
+
 }
